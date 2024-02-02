a44b506b054086a66de4c1ab524fb3f6c6adbbfa
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-02-03 12:39:29 +1300

    Adjusted how the dragon becomes bigger

--- Source/Entities/DragonBody.cs
+++ Source/Entities/DragonBody.cs
@@ -7,6 +7,7 @@ public partial class DragonBody : Node2D
 	private Skeleton2D Skeleton2D;
 	private Polygon2D Sprite2D;
 	private List<RigidBody2D> RigidBodies;
+	private List<Vector2> RigidBodyPositions;
 
 	private double alapsed;
 	// Called when the node enters the scene tree for the first time.
@@ -15,20 +16,22 @@ public partial class DragonBody : Node2D
 		this.Skeleton2D = this.GetNode<Skeleton2D>("Skeleton2D");
 		this.Sprite2D = this.GetNode<Polygon2D>("Sprite2D");
 		this.RigidBodies = new List<RigidBody2D>();
+		this.RigidBodyPositions = new List<Vector2>();
 		
 		for (int i = 0; i < this.Skeleton2D.GetChildren().Count; i++)
 		{
 			RigidBodies.Add(this.GetNode<RigidBody2D>("RigidBody2D" + i));
+			this.RigidBodyPositions.Add(this.ToLocal(RigidBodies[i].GlobalPosition));
 		}
 
-		this.alapsed = 0;
+		this.alapsed = 0.0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		alapsed += delta;
-		if (alapsed > 1.0)
+		if (alapsed > 3.0)
 		{
 			this.Expand(1.1f);
 			alapsed = 0;
@@ -40,31 +43,59 @@ public partial class DragonBody : Node2D
 		for (int i = 0; i < this.Skeleton2D.GetChildren().Count; i++)
 		{
 			this.Skeleton2D.GetNode<Bone2D>(Convert.ToString(i)).GlobalPosition = this.RigidBodies[i].GlobalPosition;
+			
+			if (i == 0)
+			{
+				continue;
+			}
+			
+			this.Skeleton2D.GetNode<Bone2D>(Convert.ToString(i)).LookAt(this.RigidBodies[i].GlobalPosition);
 		}
 	}
 
 	public void Expand(float amount)
 	{
-		foreach (Bone2D bone in Skeleton2D.GetChildren())
-		{
-			bone.Position *= amount;
-		}
+		float minHeight = float.MaxValue;
+		float maxHeight = float.MinValue;
 
-		foreach (RigidBody2D rigidBody2D in RigidBodies)
+		foreach (Bone2D bone2D in this.Skeleton2D.GetChildren())
 		{
-			rigidBody2D.Position *= amount;
+			bone2D.Scale *= amount;
+		}
+		
+		for (int i = 0; i < this.RigidBodies.Count; i++)
+		{
+			RigidBody2D rigidBody2D = this.RigidBodies[i];
+			
+			this.RigidBodyPositions[i] *= amount;
+
+			rigidBody2D.GetChild<CollisionShape2D>(0).Scale *= amount;
+
+			rigidBody2D.GlobalPosition += this.RigidBodyPositions[i];
+
+			if (this.RigidBodyPositions[i].Y < minHeight)
+			{
+				minHeight = rigidBody2D.GlobalPosition.Y;
+			}
+
+			if (this.RigidBodyPositions[i].Y > maxHeight)
+			{
+				maxHeight = rigidBody2D.GlobalPosition.Y;
+			}
 
 			if (rigidBody2D.Name.Equals("RigidBody2D0"))
 			{
 				continue;
 			}
 
-			rigidBody2D.GetChild<DampedSpringJoint2D>(1).Length *= amount;
-			rigidBody2D.GetChild<DampedSpringJoint2D>(2).Length *= amount;
-			rigidBody2D.GetChild<DampedSpringJoint2D>(3).Length *= amount;
+			for (int j = 1; j < 4; j++)
+			{
+				DampedSpringJoint2D dampedSpringJoint2D = rigidBody2D.GetChild<DampedSpringJoint2D>(j);
+				dampedSpringJoint2D.Length *= amount;
+				dampedSpringJoint2D.RestLength = dampedSpringJoint2D.Length;
+			}
 		}
-		
-		this.Sprite2D.Scale *= amount;
-		this.Position = new Vector2(this.Position.X, this.Position.Y * amount + 2);
+
+		this.GlobalPosition = new Vector2(this.GlobalPosition.X, this.GlobalPosition.Y - (maxHeight - minHeight) * 4);
 	}
 }
