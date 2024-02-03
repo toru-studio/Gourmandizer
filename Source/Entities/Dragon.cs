44202beba90e6f6b501a832d57bb9ab147fa651e
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-02-03 15:50:49 +1300

    Updated dragon to size up a bit nicer

--- Source/Entities/Dragon.cs
+++ Source/Entities/Dragon.cs
@@ -7,25 +7,42 @@ public partial class Dragon : Node2D
 
 	private Node2D DragonNeck;
 
-	private Node2D DragonBody;
+	private DragonBody DragonBody;
 
 	private Array<Node> Links;
+	
+	private double alapsed;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		this.DragonHead = this.GetNode<Node2D>("DragonHead");
 		this.DragonNeck = this.GetNode<Node2D>("DragonNeck");
-		this.DragonBody = this.GetNode<Node2D>("DragonBody");
+		this.DragonBody = this.GetNode<DragonBody>("DragonBody");
 		this.Links = this.GetNode<Node2D>("Links").GetChildren();
 
-		((PinJoint2D)this.Links[0]).NodeA = this.DragonHead.GetChild<RigidBody2D>(0).GetPath();
-		((PinJoint2D)this.Links[0]).NodeB = this.DragonNeck.GetChild<RigidBody2D>(0).GetPath();
-		((PinJoint2D)this.Links[1]).NodeA = this.DragonNeck.GetChild<RigidBody2D>(0).GetPath();
-		((PinJoint2D)this.Links[1]).NodeB = this.DragonBody.GetNode<RigidBody2D>("RigidBody2D5").GetPath();
+		((PinJoint2D)this.Links[0]).NodeA = this.DragonNeck.GetChild<RigidBody2D>(0).GetPath();
+		((PinJoint2D)this.Links[0]).NodeB = this.DragonBody.GetNode<RigidBody2D>("RigidBody2D5").GetPath();
+		
+		this.alapsed = 0.0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		alapsed += delta;
+		if (alapsed > 3.0)
+		{
+			DragonBody.Expand(1.1f);
+			this.DragonHead.Scale *= 1.1f;
+			this.DragonHead.GlobalPosition = new Vector2(this.DragonHead.GlobalPosition.X, this.DragonHead.GlobalPosition.Y - this.DragonHead.GlobalPosition.Y * 0.1f);
+			this.DragonNeck.Scale *= 1.1f;
+			this.DragonNeck.GlobalPosition = new Vector2(this.DragonNeck.GlobalPosition.X, this.DragonNeck.GlobalPosition.Y - this.DragonNeck.GlobalPosition.Y * 0.1f);
+			foreach (PinJoint2D pinJoint2D in this.Links)
+			{
+				pinJoint2D.Scale *= 1.1f;
+				pinJoint2D.GlobalPosition = new Vector2(pinJoint2D.GlobalPosition.X, pinJoint2D.GlobalPosition.Y - pinJoint2D.GlobalPosition.Y * 0.1f);
+			}
+			alapsed = 0.0f;
+		}
 	}
 }
